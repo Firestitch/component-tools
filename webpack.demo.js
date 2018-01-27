bdd07ce15e86a16fa49cd39b7b51d6b0fd340eae
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-01-27 12:48:42 +0300

    added demo config

---
+++ webpack.demo.js
@@ -0,0 +1,41 @@
+const webpackMerge = require('webpack-merge');
+
+const { dir } = require('./helpers');
+
+const ENV = 'development';
+
+let playgroundConfig = require('./webpack.playground.js');
+
+module.exports = function() {
+
+  return webpackMerge(playgroundConfig(), {
+
+    context: dir('playground'),
+
+    entry: {
+      polyfills: './polyfills.ts',
+      main: './main.ts'
+    },
+
+    output: {
+      path: dir('demo'),
+      filename: '[name].js',
+      sourceMapFilename: '[name].map',
+      chunkFilename: '[id].chunk.js'
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+      mainFields: [ 'es2015', 'browser', 'module', 'main' ],
+      extensions: ['.ts', '.js', '.scss'],
+      modules: ['node_modules']
+    },
+
+    module: {
+    },
+    plugins: [
+    ],
+
+  })
+};
