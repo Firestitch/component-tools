e6f514a3ebe851bc043a674f6bcb9753d8df91e0
Eugene Kubesh <e.kubesh@wis.eu.com>
2018-03-20 12:15:04 +0300

    FC-T74 updated examples component

--- components/examples/examples.component.ts
+++ components/examples/examples.component.ts
@@ -1,19 +1,30 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'fs-examples',
   templateUrl: 'examples.component.html'
 })
-export class FsExamplesComponent implements OnInit {
+export class FsExamplesComponent implements OnInit, AfterContentChecked {
   @Input() public title: string;
   @Input('name') public submoduleName: string;
+
+  @ViewChild('body', { read: ElementRef }) public bodyRef;
+  public examples: any = [];
 
   public loaded = false;
 
   private _submoduleUrl;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(public el: ElementRef,
+              private sanitizer: DomSanitizer) {
   }
 
   public ngOnInit() {
@@ -23,7 +34,30 @@ export class FsExamplesComponent implements OnInit {
     this.loaded = true;
   }
 
+  public ngAfterContentChecked() {
+    this.getExampleElements()
+  }
+
   get submoduleUrl() {
     return this._submoduleUrl;
+  }
+
+  public scrollTo(example) {
+    if (this.bodyRef && this.bodyRef.nativeElement && example && example.el) {
+      this.bodyRef.nativeElement.scrollTo(0, example.el.offsetTop);
+    }
+  }
+
+  private getExampleElements() {
+    this.examples = Array.from(
+      this.el.nativeElement.querySelectorAll('fs-example')
+    ).reduce((acc: any[], rowElement: any, index) => {
+      const title: string = rowElement.getAttribute('title');
+      if (title) {
+        acc.push({el: rowElement, title: title});
+      }
+
+      return acc;
+    }, []);
   }
 }
