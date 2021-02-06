469246bcf81b747a9bb5d52f8dfeb7034fb3fc7d
Raymond Gigliotti <ray@firestitch.com>
2021-02-06 11:53:58 -0500

    added recursive remove files

--- package-remove.js
+++ package-remove.js
@@ -25,16 +25,16 @@ fs.mkdirSync(args.packageDir);
 
 function rmdirRecursive(path) {
   var files = [];
-  if( fs.existsSync(path) ) {
-      files = fs.readdirSync(path);
-      files.forEach(function(file,index){
-          var curPath = path + "/" + file;
-          if(fs.lstatSync(curPath).isDirectory()) { // recurse
-            rmdirRecursive(curPath);
-          } else { // delete file
-              fs.unlinkSync(curPath);
-          }
-      });
-      fs.rmdirSync(path);
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach(function (file, index) {
+      var curPath = path + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        rmdirRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path, { recursive: true });
   }
 };
