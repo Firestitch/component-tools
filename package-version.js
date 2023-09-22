f753aaef40a60f5d97fa32781feb1965273b31bd
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 14:42:29 -0400

    12.0.18

--- package-version.js
+++ package-version.js
@@ -3,21 +3,22 @@
 var fs = require('fs');
 var prompts = require('prompts');
 const env = require('./libs/env');
-const { arg } = require('./helpers');
 
-const version = require(env.packageJsonPath()).version;
-const nextPatchVersion = version.replace(/(\d+)$/, (value, part) => { 
+const currentVersion = require(env.packageJsonPath()).version;
+
+console.log(currentVersion);
+const nextPatchVersion = currentVersion.replace(/(\d+)$/, (value, part) => { 
   return Number(part) + 1 
 });
-const nextMinorVersion = version.replace(/(\d+)\.(\d+)\.(\d+)$/, (value, major, minor) => { 
+const nextMinorVersion = currentVersion.replace(/(\d+)\.(\d+)\.(\d+)$/, (value, major, minor) => { 
   return `${major}.${Number(minor) + 1}.0`;
 });
-const nextMajorVersion = version.replace(/(\d+)\.(\d+)\.(\d+)$/, (value, major) => { 
+const nextMajorVersion = currentVersion.replace(/(\d+)\.(\d+)\.(\d+)$/, (value, major) => { 
   return `${Number(major) + 1}.0.0`;
 });
 
 (async () => {
-  const promptVersion = async () => {
+  const promptVersion = () => {
     return prompts([
       {
         type: 'select',
@@ -27,7 +28,7 @@ const nextMajorVersion = version.replace(/(\d+)\.(\d+)\.(\d+)$/, (value, major) 
           { title: `Next patch version ${nextPatchVersion}`, value: nextPatchVersion },
           { title: `Next minor version ${nextMinorVersion}`, value: nextMinorVersion },
           { title: `Next major version ${nextMajorVersion}`, value: nextMajorVersion },
-          { title: `Current version ${version}`, value: version },
+          { title: `Current version ${currentVersion}`, value: currentVersion },
           { title: 'Custom version', value: 'custom' }
         ],
         initial: 0
@@ -38,38 +39,27 @@ const nextMajorVersion = version.replace(/(\d+)\.(\d+)\.(\d+)$/, (value, major) 
         },
         name: 'version',
         message: 'Please enter the version number?',
-        initial: version,
+        initial: currentVersion,
       }
     ]);
   };
 
-  let version = null;
-  if(arg.exists('nextversion')) {
-    version = await new Promise((resolve) => {
-      const packageJson = require(env.packageJsonPath());    
-      const packageJsonVersion = packageJson.version;
-      version = packageJsonVersion.replace(/(\d+$)/, (value, part) => { 
-        return Number(part) + 1 
-      });        
-
-      resolve(version);
+  promptVersion()
+    .then((response) => {
+      const version = response.version;
+    
+      if(!version) {
+        process.console.error('Version number not specified');
+        process.exit(55);
+      }
+      
+      const packageJson = require(env.packageJsonPath());
+      const packagePackageJson = require(env.packagePackageJsonPath());
+      packagePackageJson.version = version;  
+      packageJson.version = version;  
+    
+      fs.writeFileSync(env.packageJsonPath(), JSON.stringify(packageJson,null,2).trim());  
+      fs.writeFileSync(env.packagePackageJsonPath(), JSON.stringify(packagePackageJson,null,2).trim());  
     });
 
-  } else {
-    const response = await promptVersion();
-    version = response.version;
-  }
-  
-  if(!version) {
-    process.console.error('Version number not specified');
-    process.exit(55);
-  }
-  
-  const packageJson = require(env.packageJsonPath());
-  const packagePackageJson = require(env.packagePackageJsonPath());
-  packagePackageJson.version = version;  
-  packageJson.version = version;  
-
-  fs.writeFileSync(env.packageJsonPath(), JSON.stringify(packageJson,null,2).trim());  
-  fs.writeFileSync(env.packagePackageJsonPath(), JSON.stringify(packagePackageJson,null,2).trim());  
 })();
